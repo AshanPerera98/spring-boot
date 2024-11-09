086a700c48f04c5497cd54c31f1c5fe9377a8214
Ashan Perera <ashantiwankaperera@gmail.com>
2024-11-09 13:09:29 +0530

    custom Message conponent class and annotation

---
+++ src/main/java/com/ashan_perera/content_calendar/Message.java
@@ -0,0 +1,12 @@
+package com.ashan_perera.content_calendar;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+
+    public String getMessage(){
+        return "This is the message";
+    }
+
+}
