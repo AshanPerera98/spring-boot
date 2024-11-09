5f608b6cdd9136803e3a0d5280f943901128f37a
Ashan Perera <ashantiwankaperera@gmail.com>
2024-11-09 13:19:30 +0530

    Configuration and Bean annotations

--- src/main/java/com/ashan_perera/content_calendar/Application.java
+++ src/main/java/com/ashan_perera/content_calendar/Application.java
@@ -1,17 +1,13 @@
 package com.ashan_perera.content_calendar;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
