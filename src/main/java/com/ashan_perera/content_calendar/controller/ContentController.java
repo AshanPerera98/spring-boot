4ea0b92b14068afa954fa7ce3c318c876e6e1195
Ashan Perera <ashantiwankaperera@gmail.com>
2024-11-10 11:42:08 +0530

    CROS added

--- src/main/java/com/ashan_perera/content_calendar/controller/ContentController.java
+++ src/main/java/com/ashan_perera/content_calendar/controller/ContentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.ashan_perera.content_calendar.model.Content;
 import com.ashan_perera.content_calendar.repository.ContentCollectionRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/content")
 public class ContentController {
 
