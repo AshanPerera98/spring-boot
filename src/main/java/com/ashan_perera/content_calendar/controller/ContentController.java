9f0a98dfa8c65e9d8330861fc1a7833af525c10f
Ashan Perera <ashantiwankaperera@gmail.com>
2024-11-11 14:55:15 +0530

    search endpoint using constructed mthod

--- src/main/java/com/ashan_perera/content_calendar/controller/ContentController.java
+++ src/main/java/com/ashan_perera/content_calendar/controller/ContentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -77,4 +78,9 @@ public class ContentController {
         repository.deleteById(id);
     }
 
+    // Search title by keyword
+    @GetMapping("/search")
+    public List<Content> search(@RequestParam String title){
+        return repository.findAllByTitleContains(title);
+    }
 }
--- src/main/java/com/ashan_perera/content_calendar/repository/ContentRepository.java
+++ src/main/java/com/ashan_perera/content_calendar/repository/ContentRepository.java
@@ -1,5 +1,7 @@
 package com.ashan_perera.content_calendar.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.ashan_perera.content_calendar.model.Content;
@@ -7,5 +9,8 @@ import com.ashan_perera.content_calendar.model.Content;
 // This ListCrudRepository automatically ymark this as a repository in the run time and provide all the CRUD operations.
 // first argument is the model that this repository managing and second is the data type of the id
 public interface ContentRepository extends ListCrudRepository<Content,Integer>{
+
+    // the name of the method can be constructed by nesting required expressions and it will perform the filteration automatically
+    List<Content> findAllByTitleContains(String keyword);
 
 }
